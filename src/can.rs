30499a0c69f19694b971717e04e778424bfd472a
ThatZok <af@komplix.de>
2026-02-24 22:05:27 +0100

    added PGN 56832 Reset / PGN 59392 AcknowledgmentMessage as dummy binary data frames

--- src/can.rs
+++ src/can.rs
@@ -1,18 +1,18 @@
-use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
-use embassy_sync::channel::Channel;
+use crate::komsi::komsi_task;
+use crate::time::{get_current_time_for_j1939, sync_system_time};
 use defmt::{debug, error, info, unwrap};
 use embassy_executor::Spawner;
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::channel::Channel;
 use embassy_time::{Duration, Instant, Timer};
 use embedded_can::{Frame, Id};
 use esp_hal::clock::CpuClock;
 use esp_hal::gpio::Io;
 use esp_hal::timer::timg::TimerGroup;
 use esp_hal::twai::{BaudRate, EspTwaiFrame, ExtendedId, TwaiConfiguration, TwaiMode};
+use esp_hal::twai::{TwaiRx, TwaiTx};
 use esp_hal::usb_serial_jtag::UsbSerialJtag;
 use esp_hal::Async;
-use crate::komsi::komsi_task;
-use crate::time::{get_current_time_for_j1939, sync_system_time};
-use esp_hal::twai::{TwaiTx, TwaiRx};
 
 // Kanal für 16 Frames - Puffer für "einmal alles senden"
 pub static CAN_TX_CHANNEL: Channel<CriticalSectionRawMutex, EspTwaiFrame, 16> = Channel::new();
@@ -21,7 +21,6 @@ pub static CAN_TX_CHANNEL: Channel<CriticalSectionRawMutex, EspTwaiFrame, 16> = 
 pub async fn can_send_frame(frame: EspTwaiFrame) {
     CAN_TX_CHANNEL.send(frame).await;
 }
-
 
 /// Task für das Senden (verarbeitet die Warteschlange)
 #[embassy_executor::task]
@@ -55,9 +54,49 @@ pub async fn can_rx_task(mut rx: TwaiRx<'static, Async>) {
                 // J1939 PGN extrahieren (Bits 8-25 der 29-Bit ID)
                 let pgn = (id >> 8) & 0x3FFFF;
                 let source_address = id & 0xFF;
-                info!("RX ID: ID={:08X} PGN: {:05X} von {:02X}", id, pgn, source_address);
-                
-                // TODO: Eventuell Frames an einen Verarbeitungs-Task weiterleiten
+                info!(
+                    "RX ID: ID={:08X} PGN: {:05X} von {:02X}",
+                    id, pgn, source_address
+                );
+
+                // Check auf die spezifische ID: 0x1CDEEE17
+                //   Priority: 7
+                //   Data Page (DP): 0
+                //   Parameter Group Number (PGN): Reset
+                //     Hex: 0xDE00
+                //     Dec: 56832
+                //   PDU Format: PDU1(222)
+                //   Broadcast: false
+                //   Destination Address (DA): 0xEE (238)
+                //   Source Address (SA): 0x17 (23)
+                if id == 0x1CDEEE17 {
+                    info!("Spezifischer Request (1CDEEE17) erkannt, sende Antwort...");
+                    // Response 0x1CE8FFEE
+                    //   Priority: 7
+                    //   Data Page (DP): 0
+                    //   Parameter Group Number (PGN): AcknowledgmentMessage
+                    //    Hex: 0xE800
+                    //    Dec: 59392
+                    //   PDU Format: PDU1(232)
+                    //   Broadcast: true
+                    //   Destination Address (DA): 0xFF (255)
+
+                    // ACK
+                    // "Ich habe die Anfrage für die PGN 56832 (0xDE00) erhalten und bestätige hiermit
+                    // positiv (Control Byte 0), dass ich sie verarbeite."
+
+                    let resp_id = ExtendedId::new(0x1CE8FFEE).unwrap();
+                    let resp_data = [0x00, 0xFF, 0xFF, 0xFF, 0xFF, 0x00, 0xDE, 0x00];
+                    let resp_frame = EspTwaiFrame::new(resp_id, &resp_data).unwrap();
+
+                    // if let Ok(resp_frame) = EspTwaiFrame::new(resp_id, &resp_data) {
+                    // Ab in die Warteschlange
+                    CAN_TX_CHANNEL.send(resp_frame).await;
+                    info!("Antwort-Frame 1CE8FFEE gesendet.");
+                    // }
+                }
+
+                // TODO: Eventuell Frames an einen weiteren Verarbeitungs-Task weiterleiten
             }
             Err(e) => {
                 error!("CAN RX Hardware Fehler: {:?}", e);
